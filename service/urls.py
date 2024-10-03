c40f58eb7e8ef509549b868fbeca519bb2db3bce
Amal Roshan <amalroshank786@gmail.com>
2024-10-03 13:40:43 +0530

    add: CustomerListCreateView and url

--- service/urls.py
+++ service/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 
 from rest_framework.authtoken.views import ObtainAuthToken
 
+from service import views
+
 
 urlpatterns = [
 
     path('token/',ObtainAuthToken.as_view()),
 
-    
+    path('customers/',views.CustomerListCreateView.as_view()),
 
 ]
--- service/views.py
+++ service/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 
+from rest_framework import generics,authentication,permissions
+
+from service.models import Customer
+
+from service.serializers import CustomerSerializer
+
 # Create your views here.
 
 
+class CustomerListCreateView(generics.ListCreateAPIView):
 
+    queryset = Customer.objects.all()
+
+    serializer_class = CustomerSerializer
+
+    authentication_classes = [authentication.TokenAuthentication]
+
+    permission_classes = [permissions.IsAdminUser]
+
+
+    def perform_create(self, serializer):
+
+        return serializer.save(service_advisor = self.request.user)
 
 
